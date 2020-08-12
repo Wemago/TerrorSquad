1799c255d27d189a38eb23690c2e8319e7080f5e
RickySimes <66644368+RickySimes@users.noreply.github.com>
2020-08-12 12:35:53 +0000

    Added views and get request handling

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/views/index.html');
 });
 
+app.get('/video', (req, res) => {
+    res.sendFile(__dirname + '/public/views/video.html');
+});
+
+app.get('/game', (req, res) => {
+    res.sendFile(__dirname + '/public/views/game.html');
+});
+
 app.listen(port, () => {
     console.log("Example app");
 });
