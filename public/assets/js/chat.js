f1358f631c6851d11fabcb4e2c05395bed72f9c0
Andre Philip <firecond@gmail.com>
2020-08-13 08:19:42 +0000

    Fixed video with chat bug. Renamed wrong js file name. Added favicon.

--- public/assets/js/chat.js
+++ public/assets/js/chat.js
@@ -1,5 +1,7 @@
 const socket = io('/')
 
+console.log('Made socket connection', socket)
+
 const videoGrid = document.getElementById('video-grid')
 
 const myPeer = new Peer(undefined, {
@@ -58,3 +60,33 @@ function addVideoStream(video, stream) {
     })
     videoGrid.append(video)
 }
+
+let message = document.getElementById('message')
+let handle = document.getElementById('handle')
+let btn = document.getElementById('send')
+let output = document.getElementById('output')
+let feedback = document.getElementById('feedback')
+
+//emit events
+btn.addEventListener('click', () => {
+    socket.emit('chat', {
+        message: message.value,
+        handle: handle.value
+    })
+    message.value = "";
+})
+
+//when there's a person typing, we capture the 'typing' event along with the handle name of the user doing the typing.
+message.addEventListener('keypress', () => {
+    socket.emit('typing', handle.value)
+})
+
+//listening for events
+socket.on('chat', (data) => {
+    feedback.innerHTML = '';
+    output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>'
+})
+
+socket.on('typing', (data) => {
+    feedback.innerHTML = `<p><em> ${data} is typing a message... </em></p>`
+})
